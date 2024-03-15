895b63097021c63bbda10e4524441d5944ad60de
changyukim98 <zxiop12@gmail.com>
2024-03-15 10:28:26 +0900

    Project Setting

---
+++ src/main/java/com/traveloper/tourfinder/TourFinderApplication.java
@@ -0,0 +1,13 @@
+package com.traveloper.tourfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traveloper/tourfinder/TourFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveloper.tourfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
