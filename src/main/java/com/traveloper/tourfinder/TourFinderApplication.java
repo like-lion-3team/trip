c26dba8c1cd4aca7765ce39a0aabfc831dfafb25
PJScript <pjs771188@gmail.com>
2024-03-19 15:59:00 +0900

    chore(entity):생성일, 수정일, 삭제유무 값이 들어간 베이스 엔티티 생성

--- src/main/java/com/traveloper/tourfinder/TourFinderApplication.java
+++ src/main/java/com/traveloper/tourfinder/TourFinderApplication.java
@@ -2,7 +2,10 @@ package com.traveloper.tourfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// JPA 이벤트 리스터 활성화
+@EnableJpaAuditing
 @SpringBootApplication
 public class TourFinderApplication {
 
