5adfc4871aa299726517da1f4915f5a2f63cc40b
changyukim98 <zxiop12@gmail.com>
2024-03-19 14:20:22 +0900

    Feat(auth): Member, Role 엔티티 추가

---
+++ src/main/java/com/traveloper/tourfinder/auth/entity/Member.java
@@ -0,0 +1,25 @@
+package com.traveloper.tourfinder.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uuid;
+    private String username;
+    @Setter
+    private String password;
+    @Setter
+    private String nickname;
+    private String email;
+
+    @ManyToOne
+    private Role role;
+}
