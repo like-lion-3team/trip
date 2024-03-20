a02ddf0e41139d39eb9c9a56866c5c9abd7cd478
changyukim98 <zxiop12@gmail.com>
2024-03-20 16:42:06 +0900

    fix(board): dto 수정, Board-Article 관계 수정

--- src/main/java/com/traveloper/tourfinder/board/entity/Article.java
+++ src/main/java/com/traveloper/tourfinder/board/entity/Article.java
@@ -1,9 +1,6 @@
 package com.traveloper.tourfinder.board.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,5 +20,6 @@ public class Article {
     @Setter
     private String content;
 
-
+    @ManyToOne
+    private Board board;
 }
