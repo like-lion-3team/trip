18cdf75f69426382204e03427326fa0f0251bdf4
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-03-20 16:26:31 +0900

    chore(places): Repo 수정

--- src/main/java/com/traveloper/tourfinder/place/repo/TripPlaceRepository.java
+++ src/main/java/com/traveloper/tourfinder/place/repo/TripPlaceRepository.java
@@ -2,6 +2,7 @@ package com.traveloper.tourfinder.place.repo;
 
 import com.traveloper.tourfinder.place.entity.TripPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TripPlaceRepository extends JpaRepository<Long, TripPlace> {
+public interface TripPlaceRepository extends JpaRepository<TripPlace, Long> {
 }
