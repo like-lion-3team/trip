d99ae1b4a319d46e2f888d87791fa6dd7918580c
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-03-20 16:04:39 +0900

    chore(places): 여행지관련 Controller, Service, Entity, Repo 생성

---
+++ src/main/java/com/traveloper/tourfinder/place/entity/TripPlace.java
@@ -0,0 +1,35 @@
+package com.traveloper.tourfinder.place.entity;
+
+import com.traveloper.tourfinder.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+// trip_place_ad_id 는 일단 제외
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TripPlace extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String desc;
+    private String thumbnailUrl;
+    private String naverPlaceId;
+    private String phone;
+    private String address;
+    // mapx
+    private Double lng;
+    // mapy
+    private Double lat;
+}
+
