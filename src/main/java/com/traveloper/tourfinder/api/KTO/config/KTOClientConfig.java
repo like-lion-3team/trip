7187efd44abb45243b0a1e3cb7b49088a4719626
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-03-21 15:34:20 +0900

    fix(api): Tour api service, yaml 파일 수정

--- src/main/java/com/traveloper/tourfinder/api/KTO/config/KTOClientConfig.java
+++ src/main/java/com/traveloper/tourfinder/api/KTO/config/KTOClientConfig.java
@@ -1,16 +1,10 @@
 package com.traveloper.tourfinder.api.KTO.config;
 
 import com.traveloper.tourfinder.api.KTO.service.KTOSearchService;
-import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.support.RestClientAdapter;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
