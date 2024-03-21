0d2e85c071e9ae7113985cfc565bda7b777595ee
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-03-21 16:06:55 +0900

    fix(api): KTOSerachService 수정

--- src/main/java/com/traveloper/tourfinder/api/KTO/service/KTOSearchService.java
+++ src/main/java/com/traveloper/tourfinder/api/KTO/service/KTOSearchService.java
@@ -6,5 +6,5 @@ import org.springframework.web.service.annotation.GetExchange;
 
 public interface KTOSearchService {
     @GetExchange("/B551011/KorService1/searchKeyword1")
-    String localSearch(@RequestParam Map<String, String> params);
+    Object localSearch(@RequestParam Map<String, String> params);
 }
