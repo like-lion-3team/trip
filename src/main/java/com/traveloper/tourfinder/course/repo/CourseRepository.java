4611423c95101c8957cca688d1d5498259a7eae4
changyukim98 <zxiop12@gmail.com>
2024-03-22 15:04:00 +0900

    feat(course): 코스 조회 기능

--- src/main/java/com/traveloper/tourfinder/course/repo/CourseRepository.java
+++ src/main/java/com/traveloper/tourfinder/course/repo/CourseRepository.java
@@ -1,8 +1,13 @@
 package com.traveloper.tourfinder.course.repo;
 
+import com.traveloper.tourfinder.auth.entity.Member;
 import com.traveloper.tourfinder.course.entity.Course;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CourseRepository
     extends JpaRepository<Course, Long> {
+    List<Course> findAllByMember(Member member);
 }
