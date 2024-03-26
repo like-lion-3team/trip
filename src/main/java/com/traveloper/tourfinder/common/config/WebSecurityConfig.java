d4481d484f2160e59068ec62052b1ebfe1d93c29
PJScript <pjs771188@gmail.com>
2024-03-26 16:54:20 +0900

    fix(admin):관리자 대시보드 수정

--- src/main/java/com/traveloper/tourfinder/common/config/WebSecurityConfig.java
+++ src/main/java/com/traveloper/tourfinder/common/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.traveloper.tourfinder.common.config;
 import com.traveloper.tourfinder.auth.jwt.JwtTokenFilter;
 import com.traveloper.tourfinder.auth.jwt.JwtTokenUtils;
 import com.traveloper.tourfinder.auth.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -24,7 +29,22 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.csrf(AbstractHttpConfigurer::disable)
+                // cors - vscode 로컬 라이브 서버 포트 허용
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:5500"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L); //1시간
+                        return config;
+                    }
+                }))
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "api/v1/auth/**",
