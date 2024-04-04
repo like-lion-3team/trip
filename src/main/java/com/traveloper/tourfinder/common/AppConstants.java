31ca52515c1dcb0bdcb69da842ab5d7f9d278148
changyukim98 <zxiop12@gmail.com>
2024-04-04 11:00:24 +0900

    fix(jwt): jwt 유효 시간 5분으로 설정

--- src/main/java/com/traveloper/tourfinder/common/AppConstants.java
+++ src/main/java/com/traveloper/tourfinder/common/AppConstants.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Component;
 
 /**
  * <p>공통 상수를 정의합니다.</p>
- * */
+ */
 
 public final class AppConstants {
 
-    public static final  Integer ACCESS_TOKEN_EXPIRE_SECOND = 20;
-    public static final  Integer REFRESH_TOKEN_EXPIRE_SECOND = 60 * 60 * 60;
-    public static final  Integer EMAIL_VERIFY_CODE_EXPIRE_SECOND = 60 * 5;  // 5분
-    public static final  Integer OAUTH2_AUTHORIZE_TOKEN_EXPIRE_SECOND = 60;
+    public static final Integer ACCESS_TOKEN_EXPIRE_SECOND = 60 * 5;
+    public static final Integer REFRESH_TOKEN_EXPIRE_SECOND = 60 * 60 * 60;
+    public static final Integer EMAIL_VERIFY_CODE_EXPIRE_SECOND = 60 * 5;  // 5분
+    public static final Integer OAUTH2_AUTHORIZE_TOKEN_EXPIRE_SECOND = 60;
 }
